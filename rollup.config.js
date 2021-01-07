3d512dffa1273e828168b6b3581a7c4cffe7715d
afeiship <1290657123@qq.com>
2021-01-07 10:47:07 +0800

    feat: style.scss/css to output

--- rollup.config.js
+++ rollup.config.js
@@ -2,39 +2,52 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+import scss from 'rollup-plugin-scss';
 
 import pkg from './package.json';
 
-export default {
-  input: 'src/main.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: '**/__tests__/**',
-      clean: true
-    }),
-    commonjs({
-      include: ['node_modules/**'],
-      namedExports: {
-        'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement'],
-        'node_modules/react-dom/index.js': ['render']
+export default [
+  {
+    input: 'src/components/style.scss',
+    plugins: [
+      scss({ output: 'dist/style.css' }),
+      copy({
+        targets: [{ src: 'src/components/style.scss', dest: 'dist' }]
+      })
+    ]
+  },
+  {
+    input: 'src/main.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
       }
-    })
-  ]
-};
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        exclude: '**/__tests__/**',
+        clean: true
+      }),
+      commonjs({
+        include: ['node_modules/**'],
+        namedExports: {
+          'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+          'node_modules/react-dom/index.js': ['render']
+        }
+      })
+    ]
+  }
+];
