4fa811d46a86b6f65acb0109cb89a8b2dc741313
afeiship <1290657123@qq.com>
2021-05-28 23:35:44 +0800

    todo: https://github.com/antfu/vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
+// https://github.com/antfu/vite-plugin-pwa
+
 export default defineConfig({
   root: 'public',
   build: {
