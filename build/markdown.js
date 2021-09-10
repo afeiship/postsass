85563667d03d48885707e89b9a0f919c9415f8b0
afeiship <1290657123@qq.com>
2021-09-10 11:08:09 +0800

    fix: ts-compontnt

--- build/markdown.js
+++ build/markdown.js
@@ -19,7 +19,7 @@ nx.declare({
 
       nx.replaceInFile('README.md', [
         ['__GENERATE_DAPP__', indentString(docApp, 2)],
-        ['../../src/main', '@boilerplate-scope/boilerplate-react-component']
+        ['../../src/main', '@boilerplate-scope/boilerplate-react-ts-component']
       ]);
     }
   }
