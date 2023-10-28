fa331c93b6fdd62d52ab461d29e9fb2667e8f6e4
afeiship <1290657123@qq.com>
2023-10-28 18:51:56 +0800

    style: upt build script

--- build/docs.js
+++ build/docs.js
@@ -4,13 +4,12 @@
   const gulp = require('gulp');
   const exec = require('child_process').execSync;
   const pkg = nx.absolutePackage();
-  const cmd = nx.getKey(pkg.bin, 'bin/index.js')
+  const cmd = nx.getKey(pkg.bin, 'bin/index.js');
   const $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*']
   });
 
   const usage = exec(`${cmd} -h`).toString().trim();
-
 
   gulp.task('docs', function () {
     return gulp
